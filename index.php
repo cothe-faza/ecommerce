0e1fd568384619bb46da5c0f46cbb1dfd151ed3a
cothe-faza <cs.getlink@gmail.com>
2026-02-26 10:40:45 +0700

    menu

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
         </div>
     </nav>
 
-    <!-- Categories -->
+    <!-- Menu -->
     <div class="bg-white border-b">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="flex space-x-8 overflow-x-auto py-4">
